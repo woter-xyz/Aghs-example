ad91794ce0f23988b65ebe9d43f96aceae90ddda
Bruce <bruce8.0@outlook.com>
2022-11-11 14:40:33 +0800

    Init

---
+++ Aghs-example/Aghs_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aghs_exampleApp.swift
+//  Aghs-example
+//
+//  Created by Bruce on 2022/11/11.
+//
+
+import SwiftUI
+
+@main
+struct Aghs_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
