029d59b85360e1efeda19b854e0fd8f36a05bc98
Bruce <zzzwco@outlook.com>
2022-12-01 18:45:15 +0800

    Update

--- Aghs-example/Examples/HudView.swift
+++ Aghs-example/Examples/HudView.swift
@@ -14,18 +14,31 @@ struct HudView: View {
   var body: some View {
     VStack {
       Button("Show") {
-        hud.show(disabled: true) {
-          VStack(spacing: 10) {
-            Text("Some text...")
-            DismissButton()
-          }
-          .padding(30)
-          .background(Color.orange)
-          .cornerRadius(10)
+        hud.show(style: .tip(style: .default(), position: .bottom)) {
+//          VStack(spacing: 10) {
+//            Text("Some text...")
+//            DismissButton()
+//          }
+//          .padding(30)
+//          .background(Color.orange)
+//          .cornerRadius(10)
+          
+//          ActivityIndicator()
+          
+          Text("Hello world!")
+            .padding()
+            .background(Color.white)
+            .clipShape(Capsule())
+            .shadow(radius: 10)
         }
       }
       .padding(50)
-      Spacer()
+      
+      NavigationLink {
+        Text("Hello")
+      } label: {
+        Text("Push")
+      }
     }
   }
 }
@@ -35,7 +48,7 @@ struct DismissButton: View {
   
   var body: some View {
     Button("Dismiss") {
-      hud.isPresented.toggle()
+      hud.hide(.spring())
     }
   }
 }
@@ -43,5 +56,12 @@ struct DismissButton: View {
 struct HudView_Previews: PreviewProvider {
   static var previews: some View {
     HudView()
+  }
+}
+
+struct ActivityIndicator: View {
+  var body: some View {
+    ProgressView()
+      .font(.largeTitle)
   }
 }
