482a22e115a37d6cec75c75e0a2658deed547364
Bruce <zzzwco@outlook.com>
2023-03-29 18:20:09 +0800

    Update

--- Aghs-example/Examples/UI/ToastView.swift
+++ Aghs-example/Examples/UI/ToastView.swift
@@ -9,50 +9,53 @@ import SwiftUI
 import Aghs
 
 struct ToastView: View {
-  @State private var top1 = false
-  @State private var top2 = false
-  @State private var top3 = false
-  @State private var isPrenseted2 = false
-  @State private var isPrenseted3 = false
+  @State private var top = false
+  @State private var bottom = false
+  @State private var loading = false
+  @State private var loading2 = false
   
   var body: some View {
-    VStack(spacing: 20) {
-      
-      Button("Loading") {
-        isPrenseted2.toggle()
-      }
-      
-      Button("Bottom") {
-        withAnimation {
-          isPrenseted3.toggle()
-        }
-      }
-      
-      Spacer()
+    VStack {
       HStack {
-        Button("Top1") {
-          top1 = true
+        Button("Top") {
+          top = true
         }
-        Button("Top2") {
-          top2 = true
+        
+        Button("Bottom") {
+          bottom = true
         }
-        Button("Top3") {
-          top3 = true
+        
+        Button("Loading") {
+          loading.toggle()
         }
       }
-
+      .padding(.bottom, 50)
+      
+      Button("Loading2") {
+        loading2.toggle()
+      }
+      Text("Hello")
+        .frame(width: 200, height: 100)
+        .border(.blue)
+        .ax.toast(isPresented: $loading2, style: .loading, position: .center) {
+          ProgressView()
+        }
     }
-    .ax.toast(isPresented: $top1, style: .default(duration: 1), position: .top) {
+    .frame(width: 300, height: 500)
+    .border(.red)
+    .ax.toast(isPresented: $top, style: .default(duration: 1), position: .top) {
       RoundedRectangle(cornerRadius: 10)
         .foregroundColor(.red)
         .frame(width: 100, height: 36)
-        .offset(y: 0)
+        .offset(y: 44)
     }
-    .ax.toast(isPresented: $top2, style: .default(duration: 3), position: .top) {
+    .ax.toast(isPresented: $bottom, style: .default(duration: 2), position: .bottom) {
       RoundedRectangle(cornerRadius: 10)
         .foregroundColor(.red)
         .frame(width: 100, height: 36)
-        .offset(y: 50)
+    }
+    .ax.toast(isPresented: $loading, style: .loading, position: .center) {
+      ProgressView()
     }
   }
 }
