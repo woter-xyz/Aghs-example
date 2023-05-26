a4fabba0ad0e459ff20222ca3f10a609867b9c39
Bruce <zzzwco@outlook.com>
2023-05-26 19:04:40 +0800

    Update

--- Aghs-example/Examples/ViewModifiers/HudManagerView.swift
+++ Aghs-example/Examples/ViewModifiers/HudManagerView.swift
@@ -32,10 +32,60 @@ struct HudManagerView: View {
   @EnvironmentObject private var hudManager: HudManager
   
   var body: some View {
-    Button("Show") {
+    VStack(spacing: 30) {
       
+      Button("Show 1") {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+          hudManager.show(id: 1, animation: .spring(), transition: .scale) {
+            hudView(title: "Dimiss 1", offsetY: -100) {
+              hudManager.hide(id: 1)
+            }
+          }
+        }
+      }
+      
+      Button("Show 2") {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+          hudManager.show(id: "two", animation: .linear(duration: 1), transition: .offset(x: -50), interactiveHide: true) {
+            hudView(title: "Dimiss 2", offsetY: 0) {
+              hudManager.hide(id: "two")
+            }
+          }
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+          hudManager.show(id: "three", backgroundColor: .purple.opacity(0.5)) {
+            hudView(title: "Dimiss 3", offsetY: 100) {
+              hudManager.hide(id: "three")
+            }
+          }
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+          hudManager.hide(id: 1)
+        }
+      }
+      
+      Button("Show to hide all") {
+        hudManager.show(interactiveHide: true) {
+          hudView(title: "Hide all", offsetY: 200) {
+            hudManager.hideAll()
+          }
+        }
+      }
     }
   }
+  
+  private func hudView(title: String, offsetY: CGFloat, dismiss: @escaping () -> Void) -> some View {
+    Button(title) {
+      dismiss()
+    }
+    .padding()
+    .background(Color.white)
+    .clipShape(RoundedRectangle(cornerRadius: 8))
+    .offset(y: offsetY)
+  }
+
 }
 
 struct HudManagerView_Previews: PreviewProvider {
